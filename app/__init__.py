b89c13c2082124086a3e5ef5d9f78ddbc1ead9a6
IlmiraK <ilmira.kupaeva@gmail.com>
2023-01-10 12:27:35 +0100

    second commit now connected to the correct database (profile) updated api according to the new database

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api, Resource, fields
 from pymongo import MongoClient
 from os import getenv
 from dotenv import load_dotenv
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -12,27 +13,47 @@ MONGO_URL = getenv('MONGO_URL')
 
 client = MongoClient(MONGO_URL)
 
-users = list(client.WeFriiendsUsers.users.find())
-
 api = Api(app)
-user_ns = api.namespace('/users')
+profile_ns = api.namespace('/profiles')
 
-usersModel = api.model('Users', {
+location_fields = api.model('Location', {
+    'country': fields.String,
+    'city': fields.String,
+})
+
+profiles_model = api.model('Profiles', {
+    '_id': fields.String,
+})
+
+profile_model = api.model('Profiles', {
+    '_id': fields.String,
     'userId': fields.String,
+    'gender': fields.String,
+    'reason': fields.List(fields.String),
+    'photos': fields.List(fields.String),
+    'createdAt': fields.DateTime,
+    'updatedAt': fields.DateTime,
+    'dob': fields.DateTime,
+    'zodiacSign': fields.String,
+    'location': fields.Nested(location_fields),
+
 })
 
 
-@user_ns.route('')
-class UsersView(Resource):
-    @api.marshal_with(usersModel, envelope='resource')
+@profile_ns.route('')
+class ProfilesView(Resource):
+    @api.marshal_with(profiles_model, envelope='resource')
     def get(self):
-        return users, 200
+        profiles = list(client.WeFriiendsUsers.profiles.find({}))
+        return profiles, 200
 
 
-@user_ns.route('/<int:bid>')
-class UserView(Resource):
-    def get(self, bid):
-        return users[bid], 200
+@profile_ns.route('/<string:uid>')
+class ProfileView(Resource):
+    @api.marshal_with(profile_model, envelope='resource')
+    def get(self, uid):
+        profile = list(client.WeFriiendsUsers.profiles.find({"_id": ObjectId(uid)}))
+        return profile, 200
 
 
 if __name__ == '__main__':
